2602725fdc9267e84dde7e7652fc06a72761f3f3
Mario Kapusta <mario.kapusta@ubisoft.com>
2019-11-03 18:22:43 +0000

    Initial commit.

---
+++ src/main/java/io/github/majusko/grpc/jwt/GrpcJwtSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.majusko.grpc.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcJwtSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcJwtSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/majusko/grpc/jwt/GrpcJwtSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.majusko.grpc.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcJwtSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
