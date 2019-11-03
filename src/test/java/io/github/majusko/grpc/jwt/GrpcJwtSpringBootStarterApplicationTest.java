03694e539523ae001b0e23353f2b48e42c5f8b85
Mario Kapusta <mario.kapusta@ubisoft.com>
2019-11-03 22:49:53 +0000

    Adding Travic CI.

--- src/test/java/io/github/majusko/grpc/jwt/GrpcJwtSpringBootStarterApplicationTest.java
+++ src/test/java/io/github/majusko/grpc/jwt/GrpcJwtSpringBootStarterApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GrpcJwtSpringBootStarterApplicationTests {
+public class GrpcJwtSpringBootStarterApplicationTest {
 
 	@Test
 	public void contextLoads() {
