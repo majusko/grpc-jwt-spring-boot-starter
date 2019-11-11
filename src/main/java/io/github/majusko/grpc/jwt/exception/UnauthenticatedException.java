5bfe2794f568f87c77fab491787eb8d055aeb7c7
majusko <mariokapustaa@gmail.com>
2019-11-11 22:11:47 +0000

    Improved test coverage and removed swallowed exceptions.

--- src/main/java/io/github/majusko/grpc/jwt/exception/UnauthenticatedException.java
+++ src/main/java/io/github/majusko/grpc/jwt/exception/UnauthenticatedException.java
@@ -4,4 +4,7 @@ public class UnauthenticatedException extends RuntimeException {
     public UnauthenticatedException(String message) {
         super(message);
     }
+    public UnauthenticatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
